987c8a7dfa3ef974230811a3be3b1412832187ef
t3rr0r <mail@t3rr0r.com>
2015-07-03 16:34:09 -0400

    added fetchers for medium users & collections

---
+++ fetcher/types/medium-user.js
@@ -0,0 +1,113 @@
+/* global require, module */
+
+var social = require('../../modules/social');
+var async = require('async');
+var URI = require('URIjs');
+var cheerio = require('cheerio');
+
+var request = require('request').defaults({
+    headers: {
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'
+    }
+});
+
+module.exports = {
+    re: /^(https?:\/\/)?(www\.)?medium.com\/@[^/\s]+\/?$/i,
+
+    init: function(opts) {
+	return {
+
+	    type: 'medium user',
+
+	    build: function(source, cb) {
+		var self = this;
+
+		request({
+		    method: 'GET',
+		    uri: source.url,
+		    gzip: true
+		}, function (error, response, body) {
+
+		    if (error) {
+			cb(error);
+			return;
+		    }
+
+		    source.data = {};
+		    
+		    try {
+			var globals = JSON.parse(/var GLOBALS = ([\s\S]+)\/\/ \]\]/ig.exec(body)[1]);
+			source.data = globals.embedded;
+			source.title = source.data.user.name;
+		    } catch(e) {
+			opts.log.error(e);
+		    }
+
+		    cb();
+		}).on('error', function() {}).end();
+	    },
+
+	    getStats: function(id, cb) {
+		request({
+		    method: 'GET',
+		    uri: 'https://medium.com/p/' + id + '/upvotes',
+		    gzip: true,
+		    headers: {
+			'accept': 'application/json'
+		    }
+		}, function(error, response, body) {
+		    if (error) {
+			cb(error);
+			return;
+		    }
+
+		    var count = 1;
+
+		    try {
+			var data = body.substring(16);
+			count = JSON.parse(data).payload.value.count;
+		    } catch(e) {
+			opts.log.error(e);
+		    }
+
+		    cb(null, count);
+		});
+	    },
+
+	    buildPost: function(entry, cb) {
+		var self = this;
+		var url = new URI(this.url).segment(1, entry.uniqueSlug).toString();
+		async.parallel({
+		    social: function(next) {
+			social.all(url, next);
+		    },
+		    medium: function(next) {
+			self.getStats(entry.id, next);
+		    }
+		}, function(err, results) {
+		    cb(err, {
+			title: entry.title,
+			content_url: null,
+			score: results.medium || 1,
+			social_score: results.social.total,
+			url: url
+		    });
+		});
+	    },
+
+	    getPosts: function(source, cb) {
+		source.posts = [];
+		
+		if (!source.data) {
+		    cb('missing data');
+		    return;
+		}
+
+		async.mapLimit(source.data.latestPosts, 2, this.buildPost.bind(this), function(err, results) {
+		    source.posts = results;
+		    cb(err);
+		});
+	    }
+	};
+    }
+};
