96bf7612141ff9df2cb0cd8659ca3f0f273b412d
t3rr0r <mail@t3rr0r.com>
2016-09-08 10:29:27 -0400

    cleanup

--- index.js
+++ index.js
@@ -13,38 +13,38 @@ log.info('process id', process.pid);
 
 if (cluster.isMaster) {
 
+  cluster.fork();
+
+  cluster.on('disconnect', function(worker) {
+    log.info('worker disconnect', worker);
     cluster.fork();
+  });
 
-    cluster.on('disconnect', function(worker) {
-	log.info('worker disconnect', worker);
-	cluster.fork();
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-	log.info('worker %d died, signal(%s).', worker.process.pid, signal, worker);
-    });
+  cluster.on('exit', function(worker, code, signal) {
+    log.info('worker %d died, signal(%s).', worker.process.pid, signal, worker);
+  });
 
 } else {
-    var d = domain.create();
-    var worker;
+  var d = domain.create();
+  var w;
 
-    d.on('error', function(err) {
-	log.error(err, worker.queue);
-	cluster.worker.disconnect();
+  d.on('error', function(err) {
+    log.error(err, w.queue);
+    cluster.worker.disconnect();
+  });
+
+  d.run(function() {
+    w = new Worker();
+
+    var knex = require('knex')({
+      client: 'mysql',
+      connection: config.db,
+      debug: true
     });
-
-    d.run(function() {
-	worker = new Worker();
-
-	var knex = require('knex')({
-	    client: 'mysql',
-	    connection: config.db,
-	    debug: true
-	});
-	
-	worker.init({
-	    db: knex,
-	    log: log
-	});
+    
+    w.init({
+      db: knex,
+      log: log
     });
+  });
 }
